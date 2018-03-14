adb45124f24a917b2e2231abd46800290709e411
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-03-14 22:41:30 +0100

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': "544520498171-ffjs4fgd12sg8gdskpdrbmumte9l57as.apps.googleusercontent.com",
+    'GOOGLE_CLIENT_SECRET': "bEPUWGJlMTbR-0yR-tr9hwvg",
+    'CALLBACK_URL': "http://localhost:3000/auth/google/callback"
+}
